9ef40cd76cf4f9acc81d3ac7250dd294410a08c6
Tobi Tselios <tobi.tselios@gmail.com>
2020-07-06 13:46:42 +0100

    Created MoviewCollectionViewCell and connected outlet

---
+++ PressPlay/Views/MovieCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieCollectionViewCell.swift
+//  PressPlay
+//
+//  Created by Tobi Kuyoro on 06/07/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var posterImageView: UIImageView!
+    
+}
