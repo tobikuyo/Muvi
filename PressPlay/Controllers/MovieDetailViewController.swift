c8c07d7d58256a13159e823236506699fc42d660
Tobi Tselios <tobi.tselios@gmail.com>
2020-07-10 21:19:56 +0100

    Updates in detail view controller, mostly to fetch cast details

--- PressPlay/Controllers/MovieDetailViewController.swift
+++ PressPlay/Controllers/MovieDetailViewController.swift
@@ -12,34 +12,46 @@ import Kingfisher
 class MovieDetailViewController: UIViewController {
 
     @IBOutlet var backdropImageView: UIImageView!
-    @IBOutlet var detailView: UIView!
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var runtimeLabel: UILabel!
     @IBOutlet var releaseYearLabel: UILabel!
-    @IBOutlet var genresLabel: UILabel!
+    @IBOutlet var genreLabel: UILabel!
     @IBOutlet var overviewTextView: UITextView!
     @IBOutlet var castCollectionView: UICollectionView!
 
     var movie: Movie?
+    var cast: [Cast] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         updateViews()
-    }
-
-    private func setupViews() {
-        detailView.layer.cornerRadius = 10
-        backdropImageView.layer.cornerRadius = 10
+        getCast()
     }
 
     private func updateViews() {
         guard let movie = movie else { return }
         APIController.shared.getDetails(for: movie) { movie in
             if let movie = movie {
+                let runtimeLabel = "\(String(describing: movie.runtime!)) MIN"
+                let releaseYearLabel = String((movie.releaseDate?.split(separator: "-")[0])!)
+                let genreLabel = movie.genres?.first?.name?.uppercased()
+
                 self.backdropImageView.kf.setImage(with: movie.backdrop?.url)
                 self.titleLabel.text = movie.title?.uppercased()
-                self.runtimeLabel.text = "\(String(describing: movie.runtime!)) MIN"
+                self.runtimeLabel.text = runtimeLabel
+                self.releaseYearLabel.text = releaseYearLabel
+                self.genreLabel.text = genreLabel
                 self.overviewTextView.text = movie.overview
+            }
+        }
+    }
+
+    private func getCast() {
+        guard let movie = movie else { return }
+        APIController.shared.getCast(for: movie) { data in
+            if let data = data {
+                self.cast = data.cast
+                self.castCollectionView.reloadData()
             }
         }
     }
@@ -49,5 +61,21 @@ class MovieDetailViewController: UIViewController {
     }
 
     @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+}
+
+extension MovieDetailViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cast.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CastCell", for: indexPath) as? CastCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+
+        let actor = cast[indexPath.item]
+        cell.cast = actor
+        return cell
     }
 }
