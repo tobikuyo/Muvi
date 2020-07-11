6f04114922368ceb7d725de809d56a36fbffcd5d
Tobi Tselios <tobi.tselios@gmail.com>
2020-07-11 20:34:13 +0100

    AppDelegate modifications

--- PressPlay/Application/AppDelegate.swift
+++ PressPlay/Application/AppDelegate.swift
@@ -14,7 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        UITabBar.appearance().tintColor = .red
+        let fontAttributes = [NSAttributedString.Key.font : UIFont(name: "FiraSansExtraCondensed-Regular", size: 13)!]
+        UITabBarItem.appearance().setTitleTextAttributes(fontAttributes, for: .normal)
         return true
     }
 
