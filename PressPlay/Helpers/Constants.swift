19415ce9ca3a1a4d16c7c46c7e38da50e20a25dd
Tobi Tselios <tobi.tselios@gmail.com>
2020-07-15 20:30:28 +0100

    Constants file for Firebase keys

---
+++ PressPlay/Helpers/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  PressPlay
+//
+//  Created by Tobi Kuyoro on 15/07/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import Foundation
+
+struct Fire {
+    static let images       = "images"
+    static let favourites   = "favourites"
+    static let title        = "title"
+    static let runtime      = "runtime"
+    static let releaseYear  = "releaseYear"
+    static let genre        = "genre"
+    static let overview     = "overview"
+    static let imageURL     = "imageURL"
+    static let userID       = "userID"
+    static let documentID   = "documentID"
+}
