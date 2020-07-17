a0be090f2a9a3e8adff0e1c62144c224842db98f
Tobi Tselios <tobi.tselios@gmail.com>
2020-07-17 20:33:15 +0100

    Constants file edits

--- PressPlay/Helpers/Constants.swift
+++ PressPlay/Helpers/Constants.swift
@@ -21,4 +21,27 @@ struct Fire {
     static let isSaved      = "isSaved"
     static let userID       = "userID"
     static let documentID   = "documentID"
+    static let id           = "id"
+    static let imageRef     = "imageRef"
+    static let randomID     = "randomID"
+}
+
+struct Cell {
+    static let trending   = "TrendingCell"
+    static let popular    = "PopularCell"
+    static let nowPlaying = "NowPlayingCell"
+    static let topRated   = "TopRatedCell"
+    static let cast       = "CastCell"
+    static let search     = "SearchCell"
+    static let savedMovie = "SavedMovieCell"
+}
+
+struct Font {
+    static let pathway = "PathwayGothicOne-Regular"
+    static let fira    = "FiraSansExtraCondensed-Regular"
+}
+
+struct Segue {
+    static let trending    = "TrendingMovieSegue"
+    static let movieDetail = "MovieDetailSegue"
 }
